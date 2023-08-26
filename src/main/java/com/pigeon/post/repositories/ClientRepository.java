22dc95fcf6aa00f7ccc569c276192a0c88c02c7c
liosub <mlio64565@gmail.com>
2023-08-26 23:14:44 +0100

    add mongodb configuration

---
+++ src/main/java/com/pigeon/post/repositories/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.pigeon.post.repositories;
+
+import com.pigeon.post.models.Client;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface ClientRepository extends ReactiveMongoRepository<Client,String > {
+}
