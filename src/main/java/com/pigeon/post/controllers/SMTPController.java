b157100349cc1d1d513da0fd9af0c596cf393019
liosub <mlio64565@gmail.com>
2023-08-29 17:11:43 +0100

    update imap refDB

--- src/main/java/com/pigeon/post/controllers/SMTPController.java
+++ src/main/java/com/pigeon/post/controllers/SMTPController.java
@@ -1,6 +1,8 @@
 package com.pigeon.post.controllers;
 
+import com.pigeon.post.models.Client;
 import com.pigeon.post.models.SMTPInfo;
+import com.pigeon.post.repositories.ClientRepository;
 import com.pigeon.post.repositories.SMTPRepository;
 import org.reactivestreams.Publisher;
 import org.springframework.http.HttpStatus;
@@ -12,12 +14,13 @@ import reactor.core.publisher.Mono;
 @RestController
 public class SMTPController {
     private final SMTPRepository smtpRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final ClientRepository clientRepository;
 
-    public SMTPController(SMTPRepository smtpRepository, PasswordEncoder passwordEncoder) {
+    public SMTPController(SMTPRepository smtpRepository, ClientRepository clientRepository) {
         this.smtpRepository = smtpRepository;
-        this.passwordEncoder = passwordEncoder;
+        this.clientRepository = clientRepository;
     }
+
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/api/v1/smtp")
     Flux<SMTPInfo> SMTPList(){
@@ -30,25 +33,33 @@ public class SMTPController {
     }
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/api/v1/smtp")
-    Mono<Void> createNewSMTP(@RequestBody SMTPInfo smtpInfo){
-        return smtpRepository.save(smtpInfo).then();
+    @PostMapping("/api/v1/smtp/{clientId}")
+    Mono<SMTPInfo> createNewSMTP(@PathVariable String clientId,@RequestBody SMTPInfo smtpInfo){
+        Client client  = clientRepository.findById(clientId).block();
+        if(client.getId() == null) return Mono.empty();
+        else{
+            client.addsMTP(smtpInfo);
+            Client client1 =clientRepository.save(client).block();
+        }
+        return smtpRepository.save(smtpInfo);
     }
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.OK)
     @PutMapping("/api/v1/smtp/{id}")
     Mono<SMTPInfo> updateSMTP(@PathVariable String id,@RequestBody SMTPInfo smtpInfoPublisher){
         smtpInfoPublisher.setId(id);
         return smtpRepository.save(smtpInfoPublisher);
     }
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.OK)
     @PatchMapping("api/v1/smtp/{id}")
     Mono<SMTPInfo> patchSMTP(@PathVariable String id,@RequestBody SMTPInfo smtpInfoPublisher){
         SMTPInfo smtpInfo= smtpRepository.findById(id).block();
-        if(smtpInfo.getEmail() != smtpInfoPublisher.getEmail() && smtpInfo.getPassword() != smtpInfoPublisher.getPassword()){
+        if(smtpInfo.getEmail() == smtpInfoPublisher.getEmail() && smtpInfo.getPassword() != smtpInfoPublisher.getPassword()){
             smtpInfo.setEmail(smtpInfo.getEmail());
             smtpInfo.setPassword(smtpInfo.getPassword());
-            smtpRepository.save(smtpInfo);
         }
+
+        smtpRepository.save(smtpInfo);
+
         return Mono.just(smtpInfo);
     }
 }
--- src/main/java/com/pigeon/post/controllers/iMAPController.java
+++ src/main/java/com/pigeon/post/controllers/iMAPController.java
@@ -2,6 +2,7 @@ package com.pigeon.post.controllers;
 
 import com.pigeon.post.models.Client;
 import com.pigeon.post.models.IMAPInfo;
+import com.pigeon.post.repositories.ClientRepository;
 import com.pigeon.post.repositories.IMAPRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,10 +13,13 @@ import reactor.core.publisher.Mono;
 @RestController
 public class iMAPController {
     private final IMAPRepository imapRepository;
+    private final ClientRepository clientRepository;
 
-    public iMAPController(IMAPRepository imapRepository) {
+    public iMAPController(IMAPRepository imapRepository, ClientRepository clientRepository) {
         this.imapRepository = imapRepository;
+        this.clientRepository = clientRepository;
     }
+
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/api/v1/imap")
     Flux<IMAPInfo> iMAPList(){
@@ -28,25 +32,38 @@ public class iMAPController {
     }
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/api/v1/imap")
+    @PostMapping("/api/v1/imap/{clientId}")
     @Transactional
-    Mono<IMAPInfo> createNewImap(@RequestBody IMAPInfo imapInfo){
-        return imapRepository.save(imapInfo);
+    Mono<IMAPInfo> createNewImap(@PathVariable String clientId,@RequestBody IMAPInfo imapInfo){
+        Client client  = clientRepository.findById(clientId).block();
+        if(client.getId() == null) return Mono.empty();
+        else{
+            client.addiMAPs(imapInfo);
+            Client client1 =clientRepository.save(client).block();
+        }
+       return imapRepository.save(imapInfo);
     }
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.OK)
     @PutMapping("/api/v1/imap/{id}")
     Mono<IMAPInfo> updateIMAP(@PathVariable String id,@RequestBody IMAPInfo imapInfo){
         imapInfo.setId(id);
         return imapRepository.save(imapInfo);
     }
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.OK)
     @PatchMapping("/api/v1/imap/{id}")
     Mono<IMAPInfo> patchIMAP(@PathVariable String id,@RequestBody IMAPInfo imapInfoPublisher){
         IMAPInfo imapInfo= imapRepository.findById(id).block();
-        if(imapInfo.getEmail() != imapInfoPublisher.getEmail() && imapInfo.getPassword() != imapInfoPublisher.getPassword()){
-            imapInfo.setEmail(imapInfoPublisher.getEmail());
-            imapRepository.save(imapInfo);
+
+        if(imapInfo.getPassword() != imapInfoPublisher.getPassword() && imapInfo.getEmail() == imapInfoPublisher.getEmail() ){
+            imapInfo.setPassword(imapInfoPublisher.getPassword());
         }
+        if(imapInfo.getUsage() != imapInfoPublisher.getUsage()){
+            imapInfo.setUsage(imapInfoPublisher.getUsage());
+        }
+        if(imapInfo.getStorage() != imapInfoPublisher.getStorage()){
+            imapInfo.setStorage(imapInfoPublisher.getStorage());
+        }
+        imapRepository.save(imapInfo);
         return Mono.just(imapInfo);
     }
 }
