a15ea26a5a2faf17269b6fb028ae2042f09a37a3
liosub <mlio64565@gmail.com>
2023-09-11 22:58:02 +0100

    add replyTo and forwardTo features to mail builder

--- src/main/java/com/pigeon/post/mailBuilder/Builder.java
+++ src/main/java/com/pigeon/post/mailBuilder/Builder.java
@@ -1,9 +1,9 @@
 package com.pigeon.post.mailBuilder;
 
+import com.pigeon.post.models.MailMessage;
 import com.pigeon.post.models.SMTPInfo;
 
 public interface Builder {
-    void createTextMail(SMTPInfo smtpInfo, String to, String subject, String text);
-    void createMailWithAttachment(SMTPInfo smtpInfo,String to, String subject, String text,String pathToAttachment);
-
+    void createTextMail(SMTPInfo smtpInfo, MailMessage mailMessage);
+    void createMailWithAttachment(SMTPInfo smtpInfo,MailMessage mailMessage);
 }
--- src/main/java/com/pigeon/post/mailBuilder/EmailBuilder.java
+++ src/main/java/com/pigeon/post/mailBuilder/EmailBuilder.java
@@ -1,6 +1,7 @@
 package com.pigeon.post.mailBuilder;
 
 import com.pigeon.post.mail.sender.MailSubject;
+import com.pigeon.post.models.MailMessage;
 import com.pigeon.post.models.SMTPInfo;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.MailException;
@@ -22,14 +23,21 @@ public class EmailBuilder implements Builder{
     }
 
     @Override
-    public void createTextMail(SMTPInfo smtpInfo, String to, String subject, String text) {
+    public void createTextMail(SMTPInfo smtpInfo, MailMessage mailMessage) {
         try {
 
             SimpleMailMessage message = new SimpleMailMessage();
             message.setFrom(smtpInfo.getEmail());
-            message.setTo(to);
-            message.setSubject(subject);
-            message.setText(text);
+            message.setTo(mailMessage.getTo());
+            message.setSubject(mailMessage.getSubject());
+            message.setText(mailMessage.getMessage());
+            if(mailMessage.getReplyTo() != null){
+                message.setReplyTo(mailMessage.getReplyTo());
+            }
+            if(mailMessage.getForwardTo() != null){
+                message.setBcc(mailMessage.getForwardTo());
+                message.setCc(mailMessage.getForwardTo());
+            }
             mailSubject.getObserverValue(smtpInfo.getEmail()).send(message);
 
         }
@@ -39,16 +47,23 @@ public class EmailBuilder implements Builder{
     }
 
     @Override
-    public void createMailWithAttachment(SMTPInfo smtpInfo, String to, String subject, String text, String pathToAttachment) {
+    public void createMailWithAttachment(SMTPInfo smtpInfo,MailMessage mailMessage) {
         try {
             MimeMessage message = mailSubject.getObserverValue(smtpInfo.getEmail()).createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, true);
             helper.setFrom(smtpInfo.getEmail());
-            helper.setTo(to);
-            helper.setSubject(subject);
-            helper.setText(text);
-            FileSystemResource file = new FileSystemResource(new File(pathToAttachment));
+            helper.setTo(mailMessage.getTo());
+            helper.setSubject(mailMessage.getSubject());
+            helper.setText(mailMessage.getMessage());
+            FileSystemResource file = new FileSystemResource(new File(mailMessage.getAttachmentUrl()));
             helper.addAttachment("no caption now", file);
+            if(mailMessage.getReplyTo() != null){
+                helper.setReplyTo(mailMessage.getReplyTo());
+            }
+            if(mailMessage.getForwardTo() != null){
+                helper.setBcc(mailMessage.getForwardTo());
+                helper.setCc(mailMessage.getForwardTo());
+            }
             mailSubject.getObserverValue(smtpInfo.getEmail()).send(message);
         }
         catch (MessagingException exception) {
