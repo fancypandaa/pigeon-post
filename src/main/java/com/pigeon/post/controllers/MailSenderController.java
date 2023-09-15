34fcb1996901c6a8aaa2ac8d8282230a7cea9c0c
liosub <mlio64565@gmail.com>
2023-09-15 21:59:39 +0100

    finish smtp api(s)

--- src/main/java/com/pigeon/post/controllers/MailSenderController.java
+++ src/main/java/com/pigeon/post/controllers/MailSenderController.java
@@ -3,6 +3,8 @@ package com.pigeon.post.controllers;
 import com.pigeon.post.mail.sender.MailSender;
 import com.pigeon.post.mail.sender.MailSubject;
 import com.pigeon.post.mailBuilder.EmailBuilder;
+import com.pigeon.post.models.RecipientType;
+import com.pigeon.post.models._MailMessage;
 import com.pigeon.post.models.SMTPInfo;
 import com.pigeon.post.repositories.ClientRepository;
 import com.pigeon.post.repositories.SMTPRepository;
@@ -11,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
+
 
 @RestController
 @Slf4j
@@ -28,8 +31,8 @@ public class MailSenderController extends MailSender {
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @PostMapping("/api/v1/startSmtp/{id}")
-    Mono<Void> startSmtp(@PathVariable String id){
+    @PostMapping("/api/v1/startSMTP/{id}")
+    Mono<Void> startSMTP(@PathVariable String id){
         SMTPInfo smtpInfo= smtpRepository.findById(id).block();
         log.error(smtpInfo.getEmail());
         JavaMailSender mailSender= buildMailSender(smtpInfo);
@@ -39,11 +42,38 @@ public class MailSenderController extends MailSender {
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @PostMapping("/api/v1/sendMail/{id}")
-    Mono<Void> sendMail(@PathVariable String id){
+    @PostMapping("/api/v1/closeSMTP/{id}")
+    Mono<Void> closeSMTP(@PathVariable String id){
         SMTPInfo smtpInfo= smtpRepository.findById(id).block();
         log.error(smtpInfo.getEmail());
-        emailBuilder.createTextMail(smtpInfo,"mlio64565@gmail.com","loltest","test First Enail");
+        JavaMailSender mailSender= buildMailSender(smtpInfo);
+        mailSubject.removeObserver(smtpInfo.getEmail());
+        log.error("Mail Sender has been remove from Observer");
+        return Mono.empty();
+    }
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/api/v1/sendSimpleMail/{id}")
+    Mono<Void> sendSimpleMail(@PathVariable String id, @RequestBody _MailMessage message){
+        SMTPInfo smtpInfo= smtpRepository.findById(id).block();
+        emailBuilder.createTextMail(smtpInfo, message);
+        return Mono.empty();
+    }
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/api/v1/sendMail/{id}")
+    Mono<Void> sendMail(@PathVariable String id,@PathVariable RecipientType recipientType, @ModelAttribute _MailMessage message){
+        SMTPInfo smtpInfo= smtpRepository.findById(id).block();
+        if (recipientType.equals(RecipientType.TEXT)) {
+            emailBuilder.createTextMail(smtpInfo, message);
+        }
+        else if(recipientType.equals(RecipientType.MEDIA)) {
+            if (!message.getAttachmentId().equals(null)) {
+//                download image from own bucket s3 future plan
+            } else if (!message.getAttachmentUrl().equals(null)) {
+//                download or sent by url
+            }
+
+            emailBuilder.createMailWithAttachment(smtpInfo, message);
+        }
         return Mono.empty();
     }
 }
--- src/main/java/com/pigeon/post/mailBuilder/EmailBuilder.java
+++ src/main/java/com/pigeon/post/mailBuilder/EmailBuilder.java
@@ -1,45 +1,86 @@
 package com.pigeon.post.mailBuilder;
 
+import com.pigeon.post.Services.MailMessageService;
 import com.pigeon.post.mail.sender.MailSubject;
-import com.pigeon.post.models.MailMessage;
+import com.pigeon.post.models.RecipientType;
+import com.pigeon.post.models._MailMessage;
 import com.pigeon.post.models.SMTPInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.InputStreamSource;
+import org.springframework.data.util.Pair;
 import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Component;
 
+import javax.activation.DataSource;
+import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import java.io.File;
+import javax.mail.util.ByteArrayDataSource;
+import java.io.*;
+import java.util.*;
 
 @Component
+@Slf4j
 public class EmailBuilder implements Builder{
-
+    private final int MAX_EMAIL=5;
+    private final MailMessageService mailMessageService;
     private final MailSubject mailSubject;
-    public EmailBuilder(MailSubject mailSubject) {
+
+    public EmailBuilder(MailMessageService mailMessageService, MailSubject mailSubject) {
+        this.mailMessageService = mailMessageService;
         this.mailSubject = mailSubject;
     }
-
     @Override
-    public void createTextMail(SMTPInfo smtpInfo, MailMessage mailMessage) {
+    public void createTextMail(SMTPInfo smtpInfo, _MailMessage mailMessage) {
         try {
+            mailSubject.getObserverValue(smtpInfo.getEmail()).createMimeMessage();
+            MimeMessagePreparator preparator = new MimeMessagePreparator() {
+                public void prepare(MimeMessage message) throws Exception {
+                    message.setFrom(new InternetAddress(smtpInfo.getEmail()));
+                    message.setSubject(mailMessage.getSubject());
+                    message.setText(mailMessage.getMessage());
+                    message.setRecipient(Message.RecipientType.TO,
+                            new InternetAddress(mailMessage.getTO().iterator().next()));
+                    if(mailMessage.getTO().size() > 1){
+                        for (String mail : mailMessage.getTO()) {
+                            message.addRecipients(Message.RecipientType.TO,mail);
+                        }
+                    }
+                    if(mailMessage.getCC().size() > 1){
+                        for (String mail : mailMessage.getCC()) {
+                            message.addRecipients(Message.RecipientType.CC,mail);
+                        }
+                    }
+                    if(mailMessage.getBCC().size() > 1){
+                        for (String mail : mailMessage.getBCC()) {
+                            message.addRecipients(Message.RecipientType.BCC,mail);
+                        }
+                    }
+                    if(mailMessage.getReplyTo().size()> 0){
+                        InternetAddress[] internetAddresses = {(InternetAddress) mailMessage.getReplyTo()};
+                        message.setReplyTo(internetAddresses);
+                    }
+                    if(mailMessage.getForwardTo().size()> 0){
+                        InternetAddress[] internetAddresses = {(InternetAddress) mailMessage.getForwardTo()};
+                        message.setReplyTo(internetAddresses);
+                    }
+                    message.saveChanges();
+                    log.info("MSG_ID "+message.getMessageID()+ "  DATE "+ message.getSentDate());
+                    mailMessage.setMessageId(message.getMessageID());
+                    mailMessage.setDate(message.getSentDate().toString());
+                    mailMessage.setContentType(message.getContentType());
+                    mailMessageService.createNewMail(mailMessage).block();
 
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom(smtpInfo.getEmail());
-            message.setTo(mailMessage.getTo());
-            message.setSubject(mailMessage.getSubject());
-            message.setText(mailMessage.getMessage());
-            if(mailMessage.getReplyTo() != null){
-                message.setReplyTo(mailMessage.getReplyTo());
-            }
-            if(mailMessage.getForwardTo() != null){
-                message.setBcc(mailMessage.getForwardTo());
-                message.setCc(mailMessage.getForwardTo());
-            }
-            mailSubject.getObserverValue(smtpInfo.getEmail()).send(message);
+                }
 
+            };
+
+            mailSubject.getObserverValue(smtpInfo.getEmail()).send(preparator);
         }
         catch (MailException exception) {
             exception.printStackTrace();
@@ -47,26 +88,52 @@ public class EmailBuilder implements Builder{
     }
 
     @Override
-    public void createMailWithAttachment(SMTPInfo smtpInfo,MailMessage mailMessage) {
+    public void createMailWithAttachment(SMTPInfo smtpInfo,_MailMessage mailMessage) {
         try {
             MimeMessage message = mailSubject.getObserverValue(smtpInfo.getEmail()).createMimeMessage();
+            message.setRecipient(Message.RecipientType.TO,
+                    new InternetAddress(mailMessage.getTO().iterator().next()));
+            message.setFrom(new InternetAddress(smtpInfo.getEmail()));
+            if(mailMessage.getTO().size() > 1){
+                for (String mail : mailMessage.getTO()) {
+                    message.addRecipients(Message.RecipientType.TO,mail);
+                }
+            }
+            if(mailMessage.getCC().size() > 1){
+                for (String mail : mailMessage.getCC()) {
+                    message.addRecipients(Message.RecipientType.CC,mail);
+                }
+            }
+            if(mailMessage.getBCC().size() > 1){
+                for (String mail : mailMessage.getBCC()) {
+                    message.addRecipients(Message.RecipientType.BCC,mail);
+                }
+            }
+            if(mailMessage.getReplyTo().size()> 0){
+                InternetAddress[] internetAddresses = {(InternetAddress) mailMessage.getReplyTo()};
+                message.setReplyTo(internetAddresses);
+            }
+            if(mailMessage.getForwardTo().size()> 0){
+                InternetAddress[] internetAddresses = {(InternetAddress) mailMessage.getForwardTo()};
+                message.setReplyTo(internetAddresses);
+            }
+//            FileSystemResource file = new FileSystemResource(new File("/home/fancypanda/Downloads/9aceb1c5-d077-4f5b-8e45-65f5d35bd912.JPG"));
+
             MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setFrom(smtpInfo.getEmail());
-            helper.setTo(mailMessage.getTo());
             helper.setSubject(mailMessage.getSubject());
             helper.setText(mailMessage.getMessage());
-            FileSystemResource file = new FileSystemResource(new File(mailMessage.getAttachmentUrl()));
-            helper.addAttachment("no caption now", file);
-            if(mailMessage.getReplyTo() != null){
-                helper.setReplyTo(mailMessage.getReplyTo());
-            }
-            if(mailMessage.getForwardTo() != null){
-                helper.setBcc(mailMessage.getForwardTo());
-                helper.setCc(mailMessage.getForwardTo());
-            }
+            ByteArrayDataSource file = new ByteArrayDataSource(mailMessage.getAttachment().getBytes(),mailMessage.getAttachment().getContentType());
+            helper.addAttachment(mailMessage.getAttachment().getOriginalFilename(),file );
+            message.saveChanges();
+            log.info("MSG_ID "+message.getMessageID()+ "  DATE "+ message.getSentDate());
+            mailMessage.setMessageId(message.getMessageID());
+            mailMessage.setDate(message.getSentDate().toString());
+            mailMessage.setContentType(message.getContentType());
+            mailMessageService.createNewMail(mailMessage).block();
+
             mailSubject.getObserverValue(smtpInfo.getEmail()).send(message);
         }
-        catch (MessagingException exception) {
+        catch (MessagingException | IOException exception) {
             exception.printStackTrace();
         }
     }
