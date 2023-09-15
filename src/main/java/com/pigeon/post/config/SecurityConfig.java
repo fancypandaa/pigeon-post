286a1c50d82cb5a109bca9a77f1fc997042217cd
liosub <mlio64565@gmail.com>
2023-09-15 21:58:16 +0100

    add swagger

--- src/main/java/com/pigeon/post/config/SecurityConfig.java
+++ src/main/java/com/pigeon/post/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/*").permitAll()
+                .antMatchers("/*","/swagger-resources").permitAll()
                 .anyRequest().permitAll()
                 .and().csrf().disable();
     }
