09fc5142ad7be21233b62705cb733565563b2605
liosub <mlio64565@gmail.com>
2023-10-03 21:36:22 +0100

    fix dev mode compiler

--- src/main/java/com/pigeon/post/Application.java
+++ src/main/java/com/pigeon/post/Application.java
@@ -1,16 +1,17 @@
 package com.pigeon.post;
 
+import com.pigeon.post.mail.receiver.MailReceiverConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-import javax.sql.DataSource;
 
 @SpringBootApplication(exclude = {
 		DataSourceAutoConfiguration.class,
-		org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration.class
-		})
+		org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration.class,
+})
 public class Application {
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
