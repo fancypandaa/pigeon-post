c787e0d4cc9a646c486b2ceb49275d5f7730642f
liosub <mlio64565@gmail.com>
2023-10-07 22:14:44 +0100

    fix smtp and imap default list

--- src/main/java/com/pigeon/post/repositories/MailBackBoneRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pigeon.post.repositories;
-
-import com.pigeon.post.models.MailBackBone;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface MailBackBoneRepository extends ReactiveMongoRepository<MailBackBone,String> {
-    MailBackBone findMailBackBoneByRootMessageId(String rootMessageId);
-}
